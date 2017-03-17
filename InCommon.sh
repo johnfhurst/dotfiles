af277845aed93f5ee7daa58e051a379976c57c98
johnfhurst <johnhurst@gmail.com>
2017-03-17 08:49:47 -0400

    Update InCommon.sh

--- InCommon.sh
+++ InCommon.sh
@@ -1,1 +1,11 @@
 
+#!/bin/bash
+
+if [ $1 ] ; then
+ certname=$1
+else
+ certname=$HOSTNAME
+fi
+certprovider="InCommon"
+reqdate=`/bin/date +%Y%m%d`
+sudo /usr/bin/openssl req -nodes -newkey rsa:2048 -keyout $certname.$reqdate.$certprovider.key -out $certname.$reqdate.$certprovider.csr -subj "/C=US/L=Washington/ST=District of Columbia/O=The George Washington University/OU=Division of Information Technology/CN=$certname/emailAddress=dns@gwu.edu"
